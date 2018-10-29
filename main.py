7072c99e95eea67db0ef272c70bd406a16744555
jessicag <jessicag@jpl.nasa.gov>
2018-10-28 19:33:42 -0700

    Added A* algorithms, testing still

--- main.py
+++ main.py
@@ -1,57 +1,71 @@
-"""Program that solves 8-puzzle. FIXME"""
+"""Program that solves 8-puzzle."""
 
 import operator
+import sys
 
 
 # Global value of goal state
 goal_state = ['1', '2', '3', '4', '5', '6', '7', '8', '0']
+curr_depth = 0
+num_nodes = 0
+max_queue = 0
 
 
 class Node:
     """This class stores attributes for each node."""
 
     def __init__(self, puzzle):
-        self.heuristic = 0  # if uniform cost search, this doesn't change
-        self.cost = 1  # Set to 1 for all, not used
+        self.heuristic = 0  # if uniform cost search, this doesn't change (h(n))
+        self.cost = 0  # Number of actions to get there (g(n))
         self.puzzle = puzzle
+        self.total = 0  # g(n) + h(n)
 
     def set_heuristic(self, type):
         if type == 2:
             self.heuristic = heuristic_misplaced(self.puzzle)
-        else:
+        elif type == 3:
             self.heuristic = heuristic_distance(self.puzzle)
+        self.total = self.heuristic + self.cost
+
+    def set_cost(self):
+        self.cost = curr_depth
+        self.total = self.heuristic + self.cost
 
 
-def uniform_cost_search(root):
-    """Uniform Cost Search main function."""
+def search_function(root, type):
+    """Search main function."""
 
-    flag_root = True
+    global num_nodes, max_queue
 
     # Start queue by adding root
     nodes_queue = [root]
 
-    print('Expanding state')
+    print('Initial state')
     print_puzzle(root.puzzle)
     print()
 
     while len(nodes_queue) > 0:
+        max_queue = max(max_queue, len(nodes_queue))
+
         # Get the cheapest node
         node = nodes_queue[0]
 
         if node.puzzle == goal_state:
             print('Goal!!')
+            print()
+            print('To solve this problem the search algorithm expanded a '
+                  'total of ' + str(num_nodes) + ' nodes.\nThe maximum '
+                  'number of nodes in the queue at any one time was '
+                  + str(max_queue) + '.\nThe depth of the goal node was ' + str(node.cost))
             return
-            #FIXME
 
-        if not flag_root:
-            print('The best state to expand with a g(n) = ' + str(node.cost)
-                  + ' and h(n) = ' + str(node.heuristic) + ' is...')
-            print_puzzle(node.puzzle)
-            print('Expanding this node...\n')
-        else:
-            flag_root = False
+        print('The best state to expand with a g(n) = ' + str(node.cost)
+              + ' and h(n) = ' + str(node.heuristic) + ' is...')
+        print_puzzle(node.puzzle)
+        print('Expanding this node...\n')
 
-        nodes_queue = expand(node, nodes_queue, 1)
+        num_nodes += 1
+        nodes_queue = expand(node, nodes_queue, type)
 
     print('Did not find a solution.')
     return
@@ -87,13 +101,10 @@ def go_up(puzzle, i, type):
         else:
             new_puzzle.append(puzzle[j])
 
-    print('New: ' + str(new_puzzle))
-
     new_node = Node(new_puzzle)
-
+    new_node.set_cost()
     if type > 1:
         new_node.set_heuristic(type)
-        print('New node h: ' + str(new_node.heuristic))
 
     return new_node
 
@@ -111,13 +122,10 @@ def go_down(puzzle, i, type):
         else:
             new_puzzle.append(puzzle[j])
 
-    print('New: ' + str(new_puzzle))
-
     new_node = Node(new_puzzle)
-
+    new_node.set_cost()
     if type > 1:
         new_node.set_heuristic(type)
-        print('New node h: ' + str(new_node.heuristic))
 
     return new_node
 
@@ -135,13 +143,10 @@ def go_right(puzzle, i, type):
         else:
             new_puzzle.append(puzzle[j])
 
-    print('New: ' + str(new_puzzle))
-
     new_node = Node(new_puzzle)
-
+    new_node.set_cost()
     if type > 1:
         new_node.set_heuristic(type)
-        print('New node h: ' + str(new_node.heuristic))
 
     return new_node
 
@@ -159,13 +164,10 @@ def go_left(puzzle, i, type):
         else:
             new_puzzle.append(puzzle[j])
 
-    print('New: ' + str(new_puzzle))
-
     new_node = Node(new_puzzle)
-
+    new_node.set_cost()
     if type > 1:
         new_node.set_heuristic(type)
-        print('New node h: ' + str(new_node.heuristic))
 
     return new_node
 
@@ -173,20 +175,20 @@ def go_left(puzzle, i, type):
 def expand(node, queue, search_type):
     """This function expands a given node. Then it adds these nodes to the nodes queue."""
 
+    global curr_depth
+    curr_depth = node.cost + 1
     for i in range(len(node.puzzle)):
         if node.puzzle[i] == '0':
             if i == 0:
                 node1 = go_down(node.puzzle, i, search_type)
                 node2 = go_right(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
             if i == 1:
@@ -194,29 +196,25 @@ def expand(node, queue, search_type):
                 node2 = go_right(node.puzzle, i, search_type)
                 node3 = go_left(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
                 queue.append(node3)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
             if i == 2:
                 node1 = go_down(node.puzzle, i, search_type)
                 node2 = go_left(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
             if i == 3:
@@ -224,15 +222,13 @@ def expand(node, queue, search_type):
                 node2 = go_down(node.puzzle, i, search_type)
                 node3 = go_right(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
                 queue.append(node3)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
             if i == 4:
@@ -241,7 +237,6 @@ def expand(node, queue, search_type):
                 node3 = go_right(node.puzzle, i, search_type)
                 node4 = go_left(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
@@ -249,8 +244,7 @@ def expand(node, queue, search_type):
                 queue.append(node4)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
             if i == 5:
@@ -258,29 +252,25 @@ def expand(node, queue, search_type):
                 node2 = go_down(node.puzzle, i, search_type)
                 node3 = go_left(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
                 queue.append(node3)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
             if i == 6:
                 node1 = go_up(node.puzzle, i, search_type)
                 node2 = go_right(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
             if i == 7:
@@ -288,29 +278,25 @@ def expand(node, queue, search_type):
                 node2 = go_right(node.puzzle, i, search_type)
                 node3 = go_left(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
                 queue.append(node3)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
             if i == 8:
                 node1 = go_up(node.puzzle, i, search_type)
                 node2 = go_left(node.puzzle, i, search_type)
 
-                print('Queue bef del: ' + str(len(queue)))
                 del queue[0]
                 queue.append(node1)
                 queue.append(node2)
 
                 if search_type > 1:
-                    queue.sort(key=operator.attrgetter('heuristic'))
-                print('Queue after: ' + str(len(queue)))
+                    queue.sort(key=operator.attrgetter('total'))
 
                 return queue
 
@@ -494,6 +480,39 @@ def heuristic_distance(puzzle):
     return h
 
 
+def default_puzzles():
+    """This function selects one of the default puzzles."""
+
+    print('Select a default puzzle difficulty level\n'
+          '1. Trivial\n'
+          '2. Very Easy\n'
+          '3. Easy\n'
+          '4. Doable\n'
+          '5. Oh Boy\n'
+          '6. Impossible\n'
+          '7. Report problem')
+    chosen_puzzle = input()
+    print()
+
+    if chosen_puzzle == '1':
+        return ['1', '2', '3', '4', '5', '6', '7', '8', '0']
+    elif chosen_puzzle == '2':
+        return ['1', '2', '3', '4', '5', '6', '7', '0', '8']
+    elif chosen_puzzle == '3':
+        return ['1', '2', '0', '4', '5', '3', '7', '8', '6']
+    elif chosen_puzzle == '4':
+        return ['0', '1', '2', '4', '5', '3', '7', '8', '6']
+    elif chosen_puzzle == '5':
+        return ['8', '7', '1', '6', '0', '2', '5', '4', '3']
+    elif chosen_puzzle == '6':
+        return ['1', '2', '3', '4', '5', '6', '8', '7', '0']
+    elif chosen_puzzle == '7':
+        return ['1', '2', '3', '4', '0', '6', '7', '5', '8']
+    else:
+        print('Invalid default puzzle.')
+        sys.exit()
+
+
 if __name__ == "__main__":
 
     # Intro
@@ -505,19 +524,13 @@ if __name__ == "__main__":
 
     # Get puzzle
     if puzzle_type == '1':
-        print('Default:')
-        puzzle_arr = ['1', '2', '3', '4', '5', '0', '7', '8', '6']
-        print('1 2 3\n'
-              '4 5 0\n'
-              '7 8 6\n')
-        #FIXME
+        puzzle_arr = default_puzzles()
     elif puzzle_type == '2':
         print('Enter your puzzle, use a zero to represent the blank')
         first_row = input('Enter the first row, use space or tabs between numbers: ').replace('\t', ' ')
         second_row = input('Enter the second row, use space or tabs between numbers: ').replace('\t', ' ')
         third_row = input('Enter the third row, use space or tabs between numbers: ').replace('\t', ' ')
         print()
-
         puzzle_arr = first_row.split(' ') + second_row.split(' ') + third_row.split(' ')
 
     # Get search algorithm
@@ -533,12 +546,12 @@ if __name__ == "__main__":
 
     # Call appropriate search function
     if search_type == '1':
-        uniform_cost_search(root)
+        search_function(root, 1)
     elif search_type == '2':
-        #heuristic = heuristic_misplaced(puzzle_arr)
-        pass
+        root.set_heuristic(2)
+        search_function(root, 2)
     elif search_type == '3':
-        #heuristic = heuristic_distance(puzzle_arr)
-        pass
+        root.set_heuristic(3)
+        search_function(root, 3)
     else:
         print('Invalid search type.')
